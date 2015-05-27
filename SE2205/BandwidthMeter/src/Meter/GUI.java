79a2ad6e1a78d5e85f9ead9720e8518584030a6d
Nelson Ong <contact@nelsonong.com>
2015-05-27 02:29:01 -0400

    Cleaned code and added comments for readability.

--- SE2205/BandwidthMeter/src/Meter/GUI.java
+++ SE2205/BandwidthMeter/src/Meter/GUI.java
@@ -18,14 +18,20 @@ public class GUI {
 	//TODO
 	// Make GUI look nice.
 	static JFrame window = new JFrame();
+	
+	// Create JLabels.
 	static JLabel dlLabel = new JLabel("Download Usage (Gb): ");
 	static JLabel upLabel = new JLabel("Upload Usage (Gb): ");
 	static JLabel dlRateLabel = new JLabel("Download Rate (Gb/s)");
 	static JLabel upRateLabel = new JLabel("Upload Rate (Gb/s)");
+	
+	// Create JTextFields for displaying usage and rate.
 	static JTextField dlText = new JTextField();
 	static JTextField upText = new JTextField();
 	static JTextField dlRateText = new JTextField();
 	static JTextField upRateText = new JTextField();
+	
+	// Create TaskRunner to retrieve usage and rate data.
 	public static TaskRunner taskObj = new TaskRunner();
 
 	
@@ -33,38 +39,52 @@ public class GUI {
 	{
 		//TODO
 		// Make GUI look nice.
+		
+		// Create label panel and add labels.
 		JPanel labelPanel = new JPanel(new GridLayout(4,1));
 		labelPanel.add(dlLabel);
 		labelPanel.add(upLabel);
 		labelPanel.add(dlRateLabel);
 		labelPanel.add(upRateLabel);
+		
+		// Create text panel and add usage and rate data.
 		JPanel textPanel = new JPanel(new GridLayout(4,1));
 		textPanel.add(dlText);
 		textPanel.add(upText);
 		textPanel.add(dlRateText);
 		textPanel.add(upRateText);
-		labelPanel.setBounds(0, 0, 150, 300);
+		
+		// Position label panel and text panel within GUI.
+		labelPanel.setBounds(0,0,150,300);
 		textPanel.setBounds(150,0,150,300);
+		
+		// Disable user interaction with GUI elements.
 		dlText.setEditable(false);
 		upText.setEditable(false);
 		dlRateText.setEditable(false);
 		upRateText.setEditable(false);
+		
+		// Add panels to GUI.
 		window.setLayout(new GridLayout(1,2));
 		window.add(labelPanel);
 		window.add(textPanel);
+		
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
-		window.setBounds(200, 200, 300,300 );
+		window.setBounds(200,200,300,300);
 		window.setVisible(true);
 	}
 	
 	public void update()
 	{
+		// Check system output.
+		/*
 		System.out.println("Download (Gb): " + taskObj.download);
 		System.out.println("Upload(Gb): " + taskObj.upload);
 		System.out.println("Download Rate(Gb/s): " + taskObj.downloadRate);
-		System.out.println("Upload Rate(Gb/s): " + taskObj.uploadRate);
+		System.out.println("Upload Rate(Gb/s): " + taskObj.uploadRate);*/
 		
+		// Update usage and rate data every time update() is called.
 		dlText.setText(String.valueOf(taskObj.download));
 		upText.setText(String.valueOf(taskObj.upload));
 		dlRateText.setText(String.valueOf(taskObj.downloadRate));
@@ -76,15 +96,15 @@ public class GUI {
 		final GUI GUIObj = new GUI();
 		GUIObj.initialize();
 		
-		// Handler for the timer.
+		// Event handler for the timer.
 		ActionListener timerTask = new ActionListener(){
 			public void actionPerformed(ActionEvent evt){
 				taskObj.executeNetStat();
 				GUIObj.update();
 			}
 		};
-		// Timer
-		Timer refreshTimer = new Timer(taskObj.refreshRate,timerTask);
+		// Create timer and call update() every time TaskRunner refreshes.
+		Timer refreshTimer = new Timer(taskObj.refreshRate, timerTask);
 		refreshTimer.start();	
 	}
 
